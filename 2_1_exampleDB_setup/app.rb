f3e3aca8a235e6e4254548be8b0e55033a0b44c2
Samir70 <sbetmouni@talktalk.net>
2022-11-21 17:40:28 +0000

    make print of album prettier

--- 2_1_exampleDB_setup/app.rb
+++ 2_1_exampleDB_setup/app.rb
@@ -1,15 +1,20 @@
-# file: app.rb
-
 require_relative 'lib/database_connection'
+require_relative 'lib/album_repository'
+require_relative 'lib/artist_repository'
 
 # We need to give the database name to the method `connect`.
 DatabaseConnection.connect('music_library')
 
 # Perform a SQL query on the database and get the result set.
-sql = 'SELECT id, title FROM albums;'
-result = DatabaseConnection.exec_params(sql, [])
+# sql = 'SELECT id, title FROM albums;'
+# result = DatabaseConnection.exec_params(sql, [])
+
+artist_repo = ArtistRepository.new
+album_repo = AlbumRepository.new
+artists = artist_repo.all
+albums = album_repo.all.each { |album| album.artist_name = artists[album.artist_id.to_i - 1].name}
 
 # Print out each record from the result set .
-result.each do |record|
-  p record
+albums.each do |record|
+  print record, "\n"
 end
--- 2_1_exampleDB_setup/lib/album.rb
+++ 2_1_exampleDB_setup/lib/album.rb
@@ -1,3 +1,7 @@
 class Album
-    attr_accessor :id, :title, :release_year, :artist_id
+  def to_s
+    return "The album #{@title} by #{@artist_name} was released in #{@release_year}"
+  end
+
+  attr_accessor :id, :title, :release_year, :artist_id, :artist_name
 end
