3a9701988cf2a29fe0bc484fb37c617341f847f7
Samir70 <sbetmouni@talktalk.net>
2022-11-25 10:32:59 +0000

    update blog seeds

--- 2_6_designing_two_tables/seed_blog.sql
+++ 2_6_designing_two_tables/seed_blog.sql
@@ -14,8 +14,12 @@ CREATE TABLE comments (
     on delete cascade
 );
 
-INSERT INTO posts (title, content) VALUES ('Amazing post', 'that has really dull content');
-INSERT INTO posts (title, content) VALUES ('Anaother amazing post', 'that also has really dull content');
+INSERT INTO posts (title, content) VALUES ('Post 1', 'Donald wants us to drink bleach.');
+INSERT INTO posts (title, content) VALUES ('Post 2', 'Donald knew that buying gas from Russia was wrong');
+INSERT INTO posts (title, content) VALUES ('Post 3', 'Donald is running for President');
 
 INSERT INTO comments (content, author_name, post_id) VALUES ('I was amazed at how dull this post was', 'anon78', 1);
-INSERT INTO comments (content, author_name, post_id) VALUES ('I whole heartedly agree regarding the dullness of this post', 'DonaldTrump', 1);
+INSERT INTO comments (content, author_name, post_id) VALUES ('I did not say that. The mainstream media is misquoting me in order to kill the masses', 'DonaldTrump', 1);
+INSERT INTO comments (content, author_name, post_id) VALUES ('This blog is a way better platform than twitter!', 'DonaldTrump', 1);
+INSERT INTO comments (content, author_name, post_id) VALUES ('Buy gas for £2 a year, just give bank details to crook@ocg.co.uk', 'H', 2);
+INSERT INTO comments (content, author_name, post_id) VALUES ('Buy gas from the USA and help make America great again', 'DonaldTrump', 2);
